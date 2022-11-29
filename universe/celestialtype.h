1e7cc7148808fede134025fb6001d1152f832509
QuakeIV <dwarfgaurd@gmail.com>
2022-11-29 04:41:50 -0800

    comment

--- universe/celestialtype.h
+++ universe/celestialtype.h
@@ -17,6 +17,10 @@ public:
 
     QString name;
 
+    // TODO: kindof dubious that this is a member, while spacecraft are a pointer
+    // this might in all due reality be more effecient, however, as spacecraft
+    // may indeed get to have different kinds of transforms with some regularity,
+    // whereas planets aught not to (we will see how well that holds up)
     OrbitType trajectory;
 
     uint64_t mass; //in exagrams (10^18 grams) (quadrillions (10^15) of kgs), ideally later replaced with more parameters later
